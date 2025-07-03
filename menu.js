5e1fc966860efcbb0f92da34a7b1d70d7398782c
boriska1973 <boriska1973@gmail.com>
2025-07-03 14:48:43 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -5,14 +5,14 @@ window.addEventListener('scroll', () => {
     const currentScrollPos = window.pageYOffset;
 
     if (prevScrollPos > currentScrollPos) {
-        // Scrolling up
+        
         navbar.style.top = "0";
     } else {
-        // Scrolling down
-        navbar.style.top = "-100px"; // or adjust based on your nav height
+        
+        navbar.style.top = "-100px"; 
     }
 
     prevScrollPos = currentScrollPos;
 
-    checkVisibility(); // Keep your section animation working
+    checkVisibility(); 
 });
