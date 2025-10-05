e122628b2a5233c70cb48707f954bcfb5eadbbcb
boriska1973 <boriska1973@gmail.com>
2025-10-05 18:19:29 -0400

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY === 0) {
-        navbar.style.top = '0'; // show menu
-    } else {
-        navbar.style.top = '-100px'; // hide menu
-    }
-});
